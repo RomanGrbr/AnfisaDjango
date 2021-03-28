0e844b796597053982528b769033c88f56550dd4
Roman <76010264+RomanGrbr@users.noreply.github.com>
2021-03-28 18:38:17 +0300

    Initial commit

--- icecream/views.py
+++ icecream/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from . import models
 
-def icecream_list(request):
-    db_query = models.icecream_db
-    icecreams = f'Список мороженого: {db_query}'
-    return HttpResponse(icecreams)
+def icecream_list(response):
+    icecreams = ''
+    for i in range(len(models.icecream_db)):
+        icecreams += f'{models.icecream_db[i]["name"]} :: '
+    return HttpResponse(f'Список сортов мороженого: {icecreams}')
