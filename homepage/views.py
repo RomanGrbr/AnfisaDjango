79a36a3a81c2b57a22cd2b8bf54163515db265ef
Roman <76010264+RomanGrbr@users.noreply.github.com>
2021-03-28 20:10:10 +0300

    Initial commit

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Anfisa for Friends -- Hello, world! -- ¯\(°:°)/¯')
+    return render(request, 'homepage/index.html')
