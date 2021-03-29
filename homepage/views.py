009c836c18f9f8bd2c67e6d443aff557377fb162
Roman <76010264+RomanGrbr@users.noreply.github.com>
2021-03-29 22:38:33 +0300

    Initial commit

--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from icecream.models import icecream_db
 from anfisafriends.models import friends_db
-
+from anfisafriends.services import what_weather
 
 def index(request):
     icecreams = ''
     friends = ''
-    for i in friends_db:
-        friends += f"{i}<br>"
+    for friend in friends_db:
+        city = friends_db[friend]
+        weather = what_weather(city)
+        friends += f'{friend}"::"{city}"{weather}"<br>'
 
     for i in range(len(icecream_db)):
         icecreams += f"{icecream_db[i]['name']} | <a href='icecream/{i}/'>Узнать состав</a><br>"
