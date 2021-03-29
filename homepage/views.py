f378701b8582a8fc9b289f797c292b9586afb43c
Roman <76010264+RomanGrbr@users.noreply.github.com>
2021-03-29 22:01:47 +0300

    Initial commit

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from icecream.models import icecream_db
+
+# def index(request):
+#     return render(request, 'homepage/index.html')
 
 def index(request):
-    return render(request, 'homepage/index.html')
+    icecreams = ''
+    for i in range(len(icecream_db)):
+        # Измените строку, добавляемую к icecreams
+        icecreams += f"{icecream_db[i]['name']} | <a href='icecream/{i}/'>Узнать состав</a><br>"
+    context = {
+        'icecreams': icecreams,
+    }
+    return render(request, 'homepage/index.html', context)
