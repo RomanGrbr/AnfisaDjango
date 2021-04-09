22086cc03d69defca5f481818adb61bf94a57054
Roman <76010264+RomanGrbr@users.noreply.github.com>
2021-04-09 22:57:32 +0300

    Initial commit

--- homepage/views.py
+++ homepage/views.py
@@ -10,34 +10,32 @@ def index(request):
     friends = ''
     city_weather = ''
     friend_output = ''
+    selected_icecream = ''
 
     for friend in friends_db:
         friends += (f'<input type="radio" name="friend"'
-                    f'required value="{friend}">{friend}<br>')
+                   f' required value="{friend}">{friend}<br>')
 
     for i in range(len(icecream_db)):
-        icecreams += (f'{icecream_db[i]["name"]} | '
-                      f'<a href="icecream/{i}/">Узнать состав</a><br>')
+        ice_form = (f'<input type="radio" name="icecream" required'
+                    f' value="{icecream_db[i]["name"]}">{icecream_db[i]["name"]}')
+
+        ice_link = f'<a href="icecream/{i}/">Узнать состав</a>'
+        icecreams += f'{ice_form} | {ice_link} <br>'
 
     if request.method == 'POST':
         selected_friend = request.POST['friend']
+        selected_icecream = request.POST['icecream']
         city = friends_db[selected_friend]
         weather = what_weather(city)
-        friend_output = f'{selected_friend}, тебе прислали мороженое!'
-        city_weather = f'Погода в городе {city}: {weather}'
 
-        print(f'имя друга: {selected_friend} \n'
-              f'город: {city} \n'
-              f'погода в городе: {weather} \n'
-              f'текст для вывода: {friend_output} \n'
-              f'погода для вывода: {city_weather} \n',
-              file=sys.stderr)
+        friend_output = f'{selected_friend}, тебе прислали {selected_icecream}!'
+        city_weather = f'В городе {city} погода: {weather}'
 
     context = {
         'icecreams': icecreams,
         'friends': friends,
         'friend_output': friend_output,
         'city_weather': city_weather,
-
     }
     return render(request, 'homepage/index.html', context)
